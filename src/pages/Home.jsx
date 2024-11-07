ff153f26ade8a2d2e0bd3c4482e5d80930e25a2b
Bandemiicc <mlllerena10@gmail.com>
2024-11-07 00:58:49 -0500

    adding all files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { getAllStarships } from "../services/sw-api";
+import StarShipCard from "../components/StarShipCard";
+
+const Home = () => {
+  // state, state function
+  const [starShips, setStarShips] = useState();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    //special cases: fetching data, timer, DOM
+    document.title = "Star Wars React";
+    getAllStarships(setStarShips, setError);
+  }, []);
+
+  return (
+    <div className="starshipcardcontainer">
+      {starShips ? (
+        starShips.results.map((starShip) => (
+          <StarShipCard key={starShip.name} starshipName={starShip.name} />
+        ))
+      ) : (
+        <>Loading...</>
+      )}
+      {error && <>{error}</>}
+    </div>
+    // <div>{starShips && starShips.results.map((starShip) => <li>bruh</li>)}</div>
+  );
+};
